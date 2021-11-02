01d6a251c21f42f5e1bb3306b418cfd7e3dfe55a
lukbrb <81429406+lukbrb@users.noreply.github.com>
2021-11-02 18:30:50 +0100

    Nouvelle fourchette

--- utils.py
+++ utils.py
@@ -1,13 +1,9 @@
 """ Python file containing some utils functions that are called in several other main scripts."""
-import os
-import shutil
-import numpy as np
-from unidip import UniDip
-import scipy.stats as stat
-from file_params import INFO_PATH
 import time
-import sys
-import re
+import numpy as np
+import scipy.stats as stat
+from unidip import UniDip
+from file_params import INFO_PATH
 
 
 def unimodal(dat):
@@ -43,32 +39,3 @@ def write_info(text, kind="[INFO]"):
     with open(INFO_PATH, "a") as f:
         f.write(f"{kind} - {text} - ({time.asctime()}\n)")
 
-
-def create_files(metric):
-    cwd = sys.path[0]
-    os.chdir(cwd)
-
-    paths = ['Results', 'Results/metrics', f"Results/metrics/{metric}",
-             f'Results/metrics/{metric}/_1', f'Results/metrics/{metric}/_1/UMAP',
-             f'Results/metrics/{metric}/_1/UMAP/other_markers',
-             f'Results/metrics/{metric}/_1/Clusters',
-             f'Results/metrics/{metric}/_01', f'Results/metrics/{metric}/_01/UMAP',
-             f'Results/metrics/{metric}/_01/UMAP/other_markers',
-             f'Results/metrics/{metric}/_01/Clusters']
-
-    for path in paths:
-
-        if os.path.exists(path):  # DELETES EXISTING DIRS AND RECREATE THEM
-            shutil.rmtree(path)
-        os.makedirs(path)
-
-    # BASELINE
-    files = os.listdir('files')
-
-    # ALL BASELINE FILES
-
-    baseline_files = list()
-    for file in files:
-        if re.match('BL', file):
-            baseline_files.append(file)
-    return baseline_files, files
